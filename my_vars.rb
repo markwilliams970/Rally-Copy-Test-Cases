f460f2e2d89033d8753472f6f50add98af7305dc
markwilliams <markwilliams@markw-macbook-pro.local>
2013-02-11 09:44:37 -0700

    User-specific variables definition file.

---
+++ my_vars.rb
@@ -0,0 +1,7 @@
+$my_base_url                     = "https://rally1.rallydev.com/slm"
+$my_username                     = "user@company.com"
+$my_password                     = "topsecret"
+$my_workspace                    = "My Workspace"
+$my_project                      = "My Project"
+$wsapi_version                   = "1.40"
+$filename                        = "copy_test_cases.csv"
